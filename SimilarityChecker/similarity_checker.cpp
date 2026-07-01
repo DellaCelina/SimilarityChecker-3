3acdeed1ca765462e4ccbc60401b3997df54b31c
Deokhyun Ahn <dks0554@gmail.com>
2026-07-01 15:36:57 +0900

    [refactor] Refactor similarity checker length score for normal cases

--- SimilarityChecker/similarity_checker.cpp
+++ SimilarityChecker/similarity_checker.cpp
@@ -1,16 +1,17 @@
 ﻿#include "similarity_checker.h"
 
+static constexpr int LENGTH_SCORE_MAX = 60;
+
 SimilarityChecker::SimilarityChecker(const std::string& targetStr) : targetStr(targetStr) {}
 
 int SimilarityChecker::getLengthSimilarityScore(const std::string& checkStr) const {
-    int targetStrLength = targetStr.size();
-    int checkStrLength = checkStr.size();
+    int targetStrSize = targetStr.size();
+    int checkStrSize = checkStr.size();
 
-    int strGap = std::abs(targetStrLength - checkStrLength);
-    int shortSize = std::min(targetStrLength, checkStrLength);
+    int sizeGap = std::abs(targetStrSize - checkStrSize);
+    int shortSize = std::min(targetStrSize, checkStrSize);
 
-    int gap = targetStr.size() - checkStr.size();
-    return 60 - 60 * strGap / shortSize;
+    return LENGTH_SCORE_MAX - LENGTH_SCORE_MAX * sizeGap / shortSize;
 }
 
 int SimilarityChecker::getAlphabetSimilarityScore(const std::string& checkStr) const {
